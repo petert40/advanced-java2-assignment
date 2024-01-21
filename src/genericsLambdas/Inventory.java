f865fd5fa505b8f23b1f09b012f016c3decf7e7f
petert40 <petertang224@gmail.com>
2024-01-21 13:45:56 -0500

    implement as per requirement for inventory and Product

--- src/genericsLambdas/Inventory.java
+++ src/genericsLambdas/Inventory.java
@@ -2,26 +2,45 @@ package genericsLambdas;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 
 //Add a type parameter to the Inventory class to say that it works with a generic type
-public class Inventory {
+public class Inventory<T> {
 
-//Define a list instance variable that takes a generic type parameter 
+//Define a list instance variable that takes a generic type parameter
+    private List<T> items;
 
 //Define a public constructor with no parameters that sets the list instance variable to an empty list
 //(you can choose the type of list you want)
+    public Inventory(){
+        this.items = new ArrayList<>();
+    }
 
 
 //Define a function that returns all items in the list instance variable
-
+    public List<T> getItems(){
+        return items;
+    }
 
 //Define a function that takes an item of any type as input and adds it to the list
+    public void addItem(T item){
+        this.items.add(item);
+    }
 
-//Define a funciton that takes an item of any type as input, removes it from the list, and then returns 
+//Define a function that takes an item of any type as input, removes it from the list, and then returns
 //the updated list
-
+    public List<T> removeItem(T item){
+//        return this.items.stream().filter((curr) -> !curr.equals(item)).collect(Collectors.toList());
+        items.remove(item);
+        return items;
+    }
 
 //Define a function that returns the number of items currently in the list
+
+    public int countItems(){
+        return items.size();
+    }
 
 
 }
--- src/genericsLambdas/Product.java
+++ src/genericsLambdas/Product.java
@@ -2,19 +2,27 @@ package genericsLambdas;
 
 public class Product {
 	//define the instance variables for 'name' and 'price'
-	
+	private String name;
+	private Double price;
 
 	//define a public constructor for Product that takes a name and price as input and sets the instance variables accordingly
-
+	public Product(String name, Double price){
+		this.name = name;
+		this.price = price;
+	}
 
 	public static void main(String[] args) {
 		//Make an Inventory object that holds Products
+		Inventory<Product> myInventory = new Inventory<>();
 
-		//Add as many products as you want to your Inventory Object using the addItem() method you defined 
-
+		//Add as many products as you want to your Inventory Object using the addItem() method you defined
+		myInventory.addItem(new Product("apple",2.0));
+		myInventory.addItem(new Product("banana",1.5));
+		myInventory.addItem(new Product("orange",0.5));
+//		System.out.println(myInventory.countItems());
 
 		//Your task is to calculate the total price of all products in an Inventory object called myInventory. Follow these steps:
-		//1. Call the getItems method on your inventory object to retrieve the list of all products.
+		//		//1. Call the getItems method on your inventory object to retrieve the list of all products.
 		//2. Convert this list into a stream by calling the stream method.
 		//3. Apply the mapToDouble operation to this stream. The operation should be applied to each Product object 
 		//	 	in the stream, converting it into its price. Use the lambda expression product -> product.price to 
@@ -22,10 +30,9 @@ public class Product {
 		//4. Apply the sum operation to the stream to get the total price of all products.
 		//5. Store this total price in a double variable called priceTotal.
 		//6. Print out priceTotal to the console to make sure you are properly summing your Products!
+		double priceTotal = myInventory.getItems().stream().mapToDouble(product -> product.price).sum();
+		System.out.println("The price total is " + priceTotal);
 
-		
-		
-		
 		//Helpful documentation:
 		//https://www.baeldung.com/java-stream-sum#using-streamsum-with-objects
 
